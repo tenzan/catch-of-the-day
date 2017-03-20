d0ffd7c8d6a5f9a4bbd23195d654e06dd9f8df25
Askarbek Karasasev <askar.karasaev@gmail.com>
2017-03-20 15:42:31 +0900

    18 - Persisting our State with Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyC-3RzvnV85_7H-YSi34nwxJamuBQ4NMyk",
+  authDomain: "catch-of-the-day-789f5.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-789f5.firebaseio.com"
+});
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
   constructor(){
@@ -12,11 +13,24 @@ class App extends React.Component {
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+
     // getinitialState
     this.state = {
       fishes: {},
       order: {}
-    }
+    };
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`,
+      {
+        context: this,
+        state: 'fishes'
+      });
+  }
+
+  componentWillUnmount(){
+    base.removeBinding(this.ref);
   }
 
   addFish(fish) {
